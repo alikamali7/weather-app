89064735317cff94056057ee85dca4b719f6b54c
Ali <alikamali0328@gmail.com>
2024-08-12 17:44:14 +0330

    Step 5: Refactoring & Create modal

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
-const API_KEY = "6a287d2d678716c572dc5312aaead508";
+import getWeatherData from "./utils/httpReq.js";
+import { removeModal, showModal } from "./utils/modal.js";
+
 const DAYS = [
   "Sunday",
   "Monday",
@@ -15,34 +16,10 @@ const searchButton = document.querySelector("button");
 const weatherContainer = document.getElementById("weather");
 const forecastContainer = document.getElementById("forecast");
 const locationIcon = document.getElementById("location");
-
-const getCurrentWeatherByName = async (city) => {
-  const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-const getCurrentWeatherByCoordinates = async (lat, lon) => {
-  const url = `${BASE_URL}/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-const getForecastWeatherByName = async (city) => {
-  const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
-
-const getForecastWeatherByCoordinates = async (lat, lon) => {
-  const url = `${BASE_URL}/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const json = await response.json();
-  return json;
-};
+const modalButton = document.getElementById("modal-button");
 
 const renderCurrentWeather = (data) => {
+  if (!data) return;
   const weatherJSX = `
         <h1>${data.name}, ${data.sys.country}</h1>
         <div id="main">
@@ -65,6 +42,8 @@ const getWeekDay = (date) => {
 };
 
 const renderForecastWeather = (data) => {
+  if (!data) return;
+
   forecastContainer.innerHTML = "";
   data = data.list.filter((obj) => obj.dt_txt.endsWith("12:00:00"));
   data.forEach((i) => {
@@ -86,37 +65,35 @@ const searchHandler = async () => {
   const cityName = searchInput.value;
 
   if (!cityName) {
-    alert("Please enter city name!");
+    showModal("Please enter city name!");
+    return;
   }
 
-  const currentData = await getCurrentWeatherByName(cityName);
+  const currentData = await getWeatherData("current", cityName);
   renderCurrentWeather(currentData);
-  const forecastData = await getForecastWeatherByName(cityName);
+  const forecastData = await getWeatherData("forecast", cityName);
   renderForecastWeather(forecastData);
 };
 
 const positionCallback = async (position) => {
-  const { latitude, longitude } = position.coords;
-  const currentData = await getCurrentWeatherByCoordinates(latitude, longitude);
+  const currentData = await getWeatherData("current", position.coords);
   renderCurrentWeather(currentData);
-  const forecastData = await getForecastWeatherByCoordinates(
-    latitude,
-    longitude
-  );
+  const forecastData = await getWeatherData("forecast", position.coords);;
   renderForecastWeather(forecastData);
 };
 
 const errorCallback = (error) => {
-  console.log(error);
+  showModal(error.message);
 };
 
 const locationHandler = () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(positionCallback, errorCallback);
   } else {
-    alert("Your browser dose not support geolocation");
+    showModal("Your browser dose not support geolocation");
   }
 };
 
 searchButton.addEventListener("click", searchHandler);
 locationIcon.addEventListener("click", locationHandler);
+modalButton.addEventListener("click", removeModal)
---
+++ utils/httpReq.js
@@ -0,0 +1,43 @@
+import { showModal } from "./modal.js";
+
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const API_KEY = "6a287d2d678716c572dc5312aaead508";
+
+const getWeatherData = async (type, data) => {
+    let url = null;
+
+    switch (type) {
+        case "current":
+            if (typeof data === "string") {
+                url = `${BASE_URL}/weather?q=${data}&appid=${API_KEY}&units=metric`
+            } else {
+                url = `${BASE_URL}/weather?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
+            }
+            break;
+        case "forecast":
+            if (typeof data === "string") {
+                url = `${BASE_URL}/forecast?q=${data}&appid=${API_KEY}&units=metric`
+            } else {
+                url = `${BASE_URL}/forecast?lat=${data.latitude}&lon=${data.longitude}&appid=${API_KEY}&units=metric`;
+            }
+        default:
+            url = `${BASE_URL}/weather?q=tehran&appid=${API_KEY}&units=metric`
+            break;
+    }
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        if (+json.cod === 200) {
+            return json
+        } else {
+            showModal(json.message);
+        }
+    } catch (error) {
+        showModal("An error occured when fetching data");
+    }
+
+    
+}
+
+export default getWeatherData;
